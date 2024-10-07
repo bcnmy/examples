a5c9bec4ccb492c65d1ce561ab319b0a0455c597
himanshugarg06 <garg91824@gmail.com>
2024-10-07 11:39:56 +0530

    fix readme:

--- nextjs-quickstart/pages/index.tsx
+++ nextjs-quickstart/pages/index.tsx
@@ -16,7 +16,8 @@ const Home: React.FC = () => {
             <h1>Welcome to Nexus Quickstart!</h1>
             <p>This is a simple example sending transaction using Nexus Smart Account.</p>
             {address && <p>Smart Account Address: {address}</p>}
-            <p>Next we will do a transaction using smart account, Transfer some eth to the above smart account to execute a transfer transaction. The funds will also cover gas fees for the transaction.</p>
+            {address && <p>Next, we'll execute a simple transaction, where we will transfer funds from smart account to another account. 
+                If the smart account doesn't have sufficient funds, please transfer to the smart account first. The funds will also cover gas fees for the transaction.</p>}
             {hash && <p>Transaction Hash: {hash}</p>}
         </div>
     );
