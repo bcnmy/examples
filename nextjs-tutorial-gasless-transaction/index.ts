e1376632c8e7ea50e8c7f838893918120a85687d
Himanshu garg <garg91824@gmail.com>
2024-11-06 18:57:26 +0530

    Update index.ts

--- nextjs-tutorial-gasless-transaction/index.ts
+++ nextjs-tutorial-gasless-transaction/index.ts
@@ -1,14 +1,16 @@
 import { createNexusClient, createBicoPaymasterClient } from "@biconomy/sdk";
-import { privateKeyToAccount } from "viem/accounts";
+import { privateKeyToAccount, generatePrivateKey } from "viem/accounts";
 import { baseSepolia } from "viem/chains";
 import { http, parseEther } from "viem";
 
-const privateKey = "PRIVATE_KEY";
-const paymasterUrl = "PAYMASTER_URL"
-const bundlerUrl = "https://sdk-relayer.staging.biconomy.io/api/v3/84532/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44";
+const privateKey = generatePrivateKey();
+const paymasterUrl =
+  'https://paymaster.biconomy.io/api/v2/84532/F7wyL1clz.75a64804-3e97-41fa-ba1e-33e98c2cc703';
+const bundlerUrl =
+  'https://bundler.biconomy.io/api/v3/84532/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44';
 
 export const createAccountAndSendTransaction = async () => {
-    const account = privateKeyToAccount(`0x${privateKey}`)
+    const account = privateKeyToAccount(`${privateKey}`)
     const nexusClient = await createNexusClient({
         signer: account,
         chain: baseSepolia,
@@ -19,13 +21,14 @@ export const createAccountAndSendTransaction = async () => {
     const address = await nexusClient.account.address;
     console.log("address", address)
     try {
-        const hash = await nexusClient.sendTransaction({ calls: [{to : '0xf5715961C550FC497832063a98eA34673ad7C816', value: parseEther('0.0001')}] });
+        const hash = await nexusClient.sendTransaction({ calls: [{to : '0xf5715961C550FC497832063a98eA34673ad7C816', value: parseEther('0')}] });
         console.log("Transaction hash: ", hash);
         const receipt = await nexusClient.waitForTransactionReceipt({ hash });
         console.log("Transaction receipt: ", receipt);
         return {address , hash};
     }
     catch (error) {
+        console.log(error)
         return {address}
     }
 }
