ce596760204e47ff356e709437b231e15b7add3d
himanshugarg06 <garg91824@gmail.com>
2024-11-06 21:04:33 +0530

    Add batch transactions

---
+++ nextjs-tutorial-batch-transactions/index.ts
@@ -0,0 +1,39 @@
+import { createNexusClient, createBicoPaymasterClient } from "@biconomy/sdk";
+import { privateKeyToAccount, generatePrivateKey } from "viem/accounts";
+import { baseSepolia } from "viem/chains";
+import { http, parseEther } from "viem";
+
+const privateKey = generatePrivateKey();
+const paymasterUrl =
+  'https://paymaster.biconomy.io/api/v2/84532/F7wyL1clz.75a64804-3e97-41fa-ba1e-33e98c2cc703';
+const bundlerUrl =
+  'https://bundler.biconomy.io/api/v3/84532/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44';
+
+export const createAccountAndSendTransaction = async () => {
+    const account = privateKeyToAccount(`${privateKey}`)
+    const nexusClient = await createNexusClient({
+        signer: account,
+        chain: baseSepolia,
+        transport: http(),
+        bundlerTransport: http(bundlerUrl),
+        paymaster: createBicoPaymasterClient({paymasterUrl})
+    });
+    const address = await nexusClient.account.address;
+    console.log("address", address)
+    try {
+        const hash = await nexusClient.sendTransaction({ calls: [
+          {to : '0xf5715961C550FC497832063a98eA34673ad7C816', value: parseEther('0')},
+          {to : '0x0000000000000000000000000000000000000000', data: "0x"}
+        ] });
+        console.log("Transaction hash: ", hash);
+        const receipt = await nexusClient.waitForTransactionReceipt({ hash });
+        console.log("Transaction receipt: ", receipt);
+        return {address , hash};
+    }
+    catch (error) {
+        console.log(error)
+        return {address}
+    }
+}
+
+
---
+++ nextjs-tutorial-batch-transactions/pages/index.tsx
@@ -0,0 +1,25 @@
+import { createAccountAndSendTransaction } from "../index";
+import React from 'react';
+const Home: React.FC = () => {
+    const [{address, hash}, setTransactionDetails] = React.useState({address: '', hash: ''});
+
+    React.useEffect(() => {
+        const fetchTransactionDetails = async () => {
+            const {address, hash} = await createAccountAndSendTransaction();
+            setTransactionDetails({address, hash});
+        };
+        fetchTransactionDetails();
+    }, []);
+
+    return (
+        <div>
+            <h1>Sending gasless transactions!</h1>
+            <p>This is an example transaction. Executing..... </p>
+            {address && <p>Smart Account Address: {address}</p>}
+            {address && <p>Enter the paymasterUrl in the `index.ts` file. If the smart account doesn't have sufficient funds, transfer to the smart account first. </p>}
+            {hash && <p>Transaction Hash: {hash}</p>}
+        </div>
+    );
+};
+
+export default Home;
