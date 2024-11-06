b75f7ba84c35b96ac860b2956d6f92a1d29e29b9
Himanshu garg <garg91824@gmail.com>
2024-11-06 19:07:12 +0530

    Update index.tsx

--- nextjs-tutorial-gasless-transaction/pages/index.tsx
+++ nextjs-tutorial-gasless-transaction/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = () => {
     return (
         <div>
             <h1>Sending gasless transactions!</h1>
-            <p>This is an example transaction, transferring 0.001 ETH from the smart account to a random address. </p>
+            <p>This is an example transaction. Executing..... </p>
             {address && <p>Smart Account Address: {address}</p>}
             {address && <p>Enter the paymasterUrl in the `index.ts` file. If the smart account doesn't have sufficient funds, transfer to the smart account first. </p>}
             {hash && <p>Transaction Hash: {hash}</p>}
