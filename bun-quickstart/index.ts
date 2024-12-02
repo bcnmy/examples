3fe4372fe34847baa41cb721d5cc09f6a97ac109
joepegler <joe@enso.finance>
2024-12-02 16:53:31 +0000

    chore: bun quickstart (#4)

---
+++ bun-quickstart/index.ts
@@ -0,0 +1,35 @@
+import { createNexusClient } from "@biconomy/sdk";
+import { privateKeyToAccount } from "viem/accounts";
+import { baseSepolia } from "viem/chains";
+import { http } from "viem";
+
+const privateKey = process.env.PRIVATE_KEY;
+const bundlerUrl = "https://bundler.biconomy.io/api/v3/84532/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44";
+
+export const main = async () => {
+    const account = privateKeyToAccount(`0x${privateKey}`)
+    const nexusClient = await createNexusClient({
+        signer: account,
+        chain: baseSepolia,
+        transport: http(),
+        bundlerTransport: http(bundlerUrl),
+    });
+    const address = await nexusClient.account.address;
+    console.log("address", address)
+    try {
+        const hash = await nexusClient.sendTransaction({ calls: [{to : '0xf5715961C550FC497832063a98eA34673ad7C816', value: 1n}] });
+        console.log("Transaction hash: ", hash);
+        const receipt = await nexusClient.waitForTransactionReceipt({ hash });
+        console.log("Transaction receipt: ", receipt);
+        return {address , hash};
+    }
+    catch (error) {
+        return {address}
+    }
+}
+
+
+main().then((res) => {
+    console.log(res);
+    process.exit(0);
+}).catch(console.error);
