5e5a27c9061beb919c13d46197e9518d74bbe61f
VGabriel45 <vgabrielmarian21@gmail.com>
2025-01-20 16:50:43 +0200

    chore: remove old passkey project

---
+++ nextjs-passkey-demo/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GhPzvPEUMC0WMJwSzQ7aNCE2W1VqtzoNDIQmunA2llw=\"\n}"
--- nextjs-passkey-demo/src/app/types/module.ts
+++
@@ -1,5 +0,0 @@
-export type Module = {
-    name: string;
-    address: string;
-    isActive: boolean;
-}
